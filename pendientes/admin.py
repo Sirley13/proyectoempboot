c8b1fab035f7469f9e0536e97e7bb2209aec0731
leidy <leidy.recalde995gmail.com>
2019-07-20 19:35:29 -0300

    subimos las respuestas

--- pendientes/admin.py
+++ pendientes/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Tarea #importamos el modelo
+from .models import Pregunta #importamos el modelo
 
 # Register your models here.
 
-admin.site.register(Tarea) # lo registramos
+admin.site.register(Pregunta) # lo registramos
--- pendientes/models.py
+++ pendientes/models.py
@@ -2,12 +2,29 @@ from django.db import models
 
 # Create your models here.
 
-class Tarea(models.Model): #la clase tarea hereda metodos de la clase models.Model 
-    titulo = models.CharField(max_length=100) #Campo/columna titulo de tipo "campo de caracteres" de longitud maxima de 100
-    descripcion = models.TextField(null=True, blank=True) #Campo/columna titulo de tipo Texto, los argumentos blank y null son para que el campo sea opcional
-    estado = models.BooleanField(default=False) #campo booleano . con todo esto se creo una tabla con base de datos
+class Pregunta(models.Model): #la clase tarea hereda metodos de la clase models.Model 
+    pregunta = models.CharField(max_length=100) #Campo/columna titulo de tipo "campo de caracteres" de longitud maxima de 100
+    nivel = models.IntegerField() #Campo/columna titulo de tipo Texto, los argumentos blank y null son para que el campo sea opcional
+    tips = models.TextField(null=True, blank=True) #campo booleano . con todo esto se creo una tabla con base de datos
 
     def __str__(self):
-        return "tarea:" + self.titulo +" "+ str(self.estado)
+        return "preguntas:" + self.pregunta 
+
+class Respuestas(models.Model):
+    nombre_idea = models.CharField(max_length=100)
+    numero_idea = models.IntegerField()
+    calificacion =models.IntegerField()
+    email = models.EmailField(null=True, blank=True)
+    telefono = models.CharField(max_length=20,null=True, blank=True)
+    pregunta = models.ForeignKey(Pregunta, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "Respuesta:" + self.nombre_idea 
+
+
+
+
+
+
 
 
