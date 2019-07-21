ac2679fdda7a50286fbf2a19b6ea4532986e3c91
leidy <leidy.recalde995gmail.com>
2019-07-20 21:34:36 -0300

    se agrego respuestas a admin

--- pendientes/admin.py
+++ pendientes/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Pregunta #importamos el modelo
+from .models import Pregunta, Respuestas #importamos el modelo
 
 # Register your models here.
 
 admin.site.register(Pregunta) # lo registramos
+admin.site.register(Respuestas)
